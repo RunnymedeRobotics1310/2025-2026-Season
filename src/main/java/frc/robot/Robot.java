8f1744d8f596eb568ec4b75cc0cecc5d53e184e1
Richard McMullin <richard.e.mcmullin@gmail.com>
2025-11-20 07:51:35 -0500

    position and velocity display

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,8 +4,15 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
@@ -17,9 +24,21 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * this project, you must also update the Main.java file in the project.
  */
 public class Robot extends TimedRobot {
+
+  private static final double DEADBAND = 0.2;
+  private static final double SLOW_X = 0.7;
+  private static final double SLOW_Y = .4;
+
+  private static final double SLOW_M = SLOW_Y / (SLOW_X - DEADBAND);
+  private static final double SLOW_B = -SLOW_M * DEADBAND;
+
+  private static final double FAST_M = (1.0 - SLOW_Y) / (1.0 - SLOW_X);
+  private static final double FAST_B = -(FAST_M * SLOW_X) + SLOW_Y;
+
   private XboxController xboxController;
-  private SparkMax speedMotor;
+  private SparkMax driveMotor;
   private SparkMax turnMotor;
+  private CANcoder angleEncoder;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -29,10 +48,28 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
-    xboxController = new XboxController(0); // Initialize XboxController on port 0
-    speedMotor =
-        new SparkMax(30, MotorType.kBrushless); // Initialize SparkMax on port 30 for NEO motor
+    xboxController = new XboxController(0);
+
+    // Reset the speed controllers
+    SparkMaxConfig sparkMaxConfig = new SparkMaxConfig();
+    sparkMaxConfig.encoder.positionConversionFactor(1.0);
+    sparkMaxConfig.encoder.velocityConversionFactor(1.0);
+    sparkMaxConfig.inverted(false);
+    sparkMaxConfig.idleMode(IdleMode.kBrake);
+
+    driveMotor = new SparkMax(30, MotorType.kBrushless);
+    driveMotor.configure(
+        sparkMaxConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
     turnMotor = new SparkMax(31, MotorType.kBrushless);
+    turnMotor.configure(
+        sparkMaxConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    angleEncoder = new CANcoder(32);
+    CANcoderConfiguration cancoderConfig = new CANcoderConfiguration();
+    cancoderConfig.MagnetSensor.MagnetOffset = 0.0;
+    cancoderConfig.MagnetSensor.SensorDirection = SensorDirectionValue.Clockwise_Positive;
+    angleEncoder.getConfigurator().apply(cancoderConfig);
   }
 
   /**
@@ -43,7 +80,20 @@ public class Robot extends TimedRobot {
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    // Display the current motor speed and position
+    SmartDashboard.putNumber(
+        "Angle Encoder",
+        Math.round(angleEncoder.getAbsolutePosition().getValueAsDouble() * 100) / 100.0);
+    SmartDashboard.putNumber(
+        "Turn Motor Speed", Math.round(turnMotor.getEncoder().getVelocity() * 100) / 100.0);
+    SmartDashboard.putNumber(
+        "Turn Motor Position", Math.round(turnMotor.getEncoder().getPosition() * 100) / 100.0);
+    SmartDashboard.putNumber(
+        "Drive Motor Speed", Math.round(driveMotor.getEncoder().getVelocity() * 100) / 100.0);
+    SmartDashboard.putNumber(
+        "Drive Motor Position", Math.round(driveMotor.getEncoder().getPosition() * 100) / 100.0);
+  }
 
   /** This function is called once when autonomous is enabled. */
   @Override
@@ -60,16 +110,16 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    double leftY = -xboxController.getLeftY(); // Example: Get left joystick Y-axis value
-    double speed = deadband(leftY);
-    speedMotor.set(speed); // Set motor speed based on joystick input
 
-    double rightX = xboxController.getRightX(); // Example: Get right joystick X-axis value
+    // Set the drive speed based on the left Y axis with deadband applied
+    double leftY = -xboxController.getLeftY();
+    double speed = deadband(leftY);
+    driveMotor.set(speed);
+
+    // Set the turn speed based on the right X axis with deadband applied
+    double rightX = xboxController.getRightX();
     double turn = deadband(rightX);
     turnMotor.set(turn);
-
-    SmartDashboard.putNumber("turnMotor", rightX);
-    SmartDashboard.putNumber("speedMotor", leftY);
   }
 
   /** This function is called once when the robot is disabled. */
@@ -98,12 +148,14 @@ public class Robot extends TimedRobot {
 
   private double deadband(double value) {
 
-    if (Math.abs(value) < 0.2) {
+    if (Math.abs(value) < DEADBAND) {
       return 0.0;
     }
 
-    // FIXME: Implement a better deadband function
+    if (Math.abs(value) < SLOW_X) {
+      return (Math.abs(value) * SLOW_M + SLOW_B) * Math.signum(value);
+    }
 
-    return value;
+    return (Math.abs(value) * FAST_M + FAST_B) * Math.signum(value);
   }
 }
